1bf89ab85231fe91a886b4eae795b657b86d32c1
BunthongRorn <bunthongran579@gmail.com>
2025-01-15 19:43:30 +0700

    Project set up

---
+++ src/test/java/com/bunthong_job_portal/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunthong_job_portal.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
