694c235a2bab166ccabef72462101a61a843bbe2
BunthongRorn <bunthongran579@gmail.com>
2025-01-21 17:58:06 +0700

    Custom authentication success handler

--- src/main/java/com/bunthong_job_portal/jobportal/services/UsersService.java
+++ src/main/java/com/bunthong_job_portal/jobportal/services/UsersService.java
@@ -1,4 +1,5 @@
 package com.bunthong_job_portal.jobportal.services;
+
 import com.bunthong_job_portal.jobportal.entity.JobSeekerProfile;
 import com.bunthong_job_portal.jobportal.entity.RecruiterProfile;
 import com.bunthong_job_portal.jobportal.entity.Users;
@@ -6,33 +7,39 @@ import com.bunthong_job_portal.jobportal.repository.JobSeekerProfileRepository;
 import com.bunthong_job_portal.jobportal.repository.RecruiterProfileRepository;
 import com.bunthong_job_portal.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.Optional;
+
 @Service
 public class UsersService {
 
     private final UsersRepository usersRepository;
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository) {
+    public UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository, PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users users) {
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
         Users savedUser = usersRepository.save(users);
         int userTypeId = users.getUserTypeId().getUserTypeId();
 
         if (userTypeId == 1) {
             recruiterProfileRepository.save(new RecruiterProfile(savedUser));
-        } else {
+        }
+        else {
             jobSeekerProfileRepository.save(new JobSeekerProfile(savedUser));
         }
 
@@ -41,6 +48,6 @@ public class UsersService {
 
     public Optional<Users> getUserByEmail(String email) {
         return usersRepository.findByEmail(email);
-
     }
+
 }
