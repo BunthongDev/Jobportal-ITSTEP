1fa3ff377ffdb173cfcd78f76dbd81eae2d8c915
BunthongRorn <bunthongran579@gmail.com>
2025-01-25 15:24:36 +0700

    add post new job

--- src/main/java/com/bunthong_job_portal/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/bunthong_job_portal/jobportal/controller/JobPostActivityController.java
@@ -1,20 +1,25 @@
 package com.bunthong_job_portal.jobportal.controller;
 
 import com.bunthong_job_portal.jobportal.entity.JobPostActivity;
+import com.bunthong_job_portal.jobportal.entity.RecruiterJobsDto;
+import com.bunthong_job_portal.jobportal.entity.RecruiterProfile;
 import com.bunthong_job_portal.jobportal.entity.Users;
 import com.bunthong_job_portal.jobportal.services.JobPostActivityService;
 import com.bunthong_job_portal.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
@@ -37,6 +42,10 @@ public class JobPostActivityController {
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile) currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
 
         model.addAttribute("user", currentUserProfile);
@@ -60,7 +69,16 @@ public class JobPostActivityController {
         }
         jobPostActivity.setPostedDate(new Date());
         model.addAttribute("jobPostActivity", jobPostActivity);
-        jobPostActivityService.addNew(jobPostActivity);
+        JobPostActivity saved = jobPostActivityService.addNew(jobPostActivity);
         return "redirect:/dashboard/";
+    }
+
+    @PostMapping ("dashboard/edit/{id}")
+    public String editJob(@PathVariable("id") int id, Model model) {
+
+        JobPostActivity jobPostActivity = jobPostActivityService.getOne(id);
+        model.addAttribute("jobPostActivity", jobPostActivity);
+        model.addAttribute("user", usersService.getCurrentUserProfile());
+        return "add-jobs";
     }
 }
